75722db709d33867599b2d55605522d0cd4be1f8
Aleksandr Schemelev <prophreaker@gmail.com>
2023-04-03 21:09:45 +0300

    feat: add auth module with auth controller

---
+++ src/auth/api/controllers/auth.controller.ts
@@ -0,0 +1,45 @@
+import { Controller, Post } from '@nestjs/common';
+import { API } from 'src/@common/constants';
+
+@Controller(API.AUTH)
+export class AuthController {
+  @Post(API.LOGIN)
+  public async login() {
+    return null;
+  }
+
+  @Post(API.REGISTRATION)
+  public async registration() {
+    return null;
+  }
+
+  @Post(API.CONFIRM_REGISTRATION)
+  public async confirmRegistration() {
+    return null;
+  }
+
+  @Post(API.REFRESH_TOKEN)
+  public async refreshToken() {
+    return null;
+  }
+
+  @Post(API.RESEND_REGISTRATION_CONFIRMATION)
+  public async resendRegistrationConfirmation() {
+    return null;
+  }
+
+  @Post(API.PASSWORD_RECOVERY)
+  public async passwordRecovery() {
+    return null;
+  }
+
+  @Post(API.NEW_PASSWORD)
+  public async newPassword() {
+    return null;
+  }
+
+  @Post(API.LOGOUT)
+  public async logout() {
+    return null;
+  }
+}
