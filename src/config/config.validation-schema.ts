c903cb3cd17f32ff798aa50949b75313026e4bf5
Aleksandr Schemelev <prophreaker@gmail.com>
2023-04-12 00:45:11 +0300

    feat: add deploy workflow for deploying on heroku

--- src/config/config.validation-schema.ts
+++ src/config/config.validation-schema.ts
@@ -2,7 +2,6 @@ import Joi from 'joi';
 
 export const configValidationSchema = Joi.object({
   DATABASE_URL: Joi.string().required(),
-  SHADOW_DATABASE_URL: Joi.string().required(),
   MAILER_USER: Joi.string().required(),
   MAILER_PASS: Joi.string().required(),
   MAILER_HOST: Joi.string().required(),
